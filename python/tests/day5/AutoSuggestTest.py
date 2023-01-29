bf3a0b1f8b7ec08a3ea0a4839741457c59c35bfb
azatbayram <bayramov.azat.tkm@gmail.com>
2023-01-29 04:53:02 +0300

    autosuggest

---
+++ python/tests/day5/AutoSuggestTest.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import Keys, ActionChains
+
+class AutoSuggestTest():
+
+    driver=webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.yatra.com/")
+    departFrom=driver.find_element(By.XPATH, "//input[@id='BE_flight_origin_city']")
+    departFrom.click()
+    departFrom.send_keys("New Delhi")
+    ActionChains(driver).key_down(Keys.ENTER).perform()
+
+
+    time.sleep(5)
+    driver.quit()
