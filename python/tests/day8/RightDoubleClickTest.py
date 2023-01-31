8e073438588078983b4911c81bb937e91c72e0f6
azatbayram <bayramov.azat.tkm@gmail.com>
2023-01-31 04:12:12 +0300

    right and double click

---
+++ python/tests/day8/RightDoubleClickTest.py
@@ -0,0 +1,27 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+class RightDoubleClickTest():
+
+    driver=webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://demo.guru99.com/test/simple_context_menu.html")
+
+    action=ActionChains(driver)
+    rightClick=driver.find_element(By.CSS_SELECTOR, ".context-menu-one.btn.btn-neutral")
+    doubleClick=driver.find_element(By.XPATH, "(//button)[1]")
+
+    #Right Click
+    action.context_click(rightClick).perform()
+    time.sleep(2)
+
+    #Double Click
+    #action.double_click(doubleClick).perform()
+
+
+    time.sleep(3)
+    driver.quit()
